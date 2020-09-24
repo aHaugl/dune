2b2edd332a434272c9ffb940e9abdb1bd6928c5f
aHaugl <andreas.haugland1@gmail.com>
2020-09-24 17:52:50 +0200

    Monitors/Activatable: Created task (Exercise 3).

--- src/Monitors/Activatable/Task.cpp
+++ src/Monitors/Activatable/Task.cpp
@@ -24,7 +24,7 @@
 // https://github.com/LSTS/dune/blob/master/LICENCE.md and                  *
 // http://ec.europa.eu/idabc/eupl.html.                                     *
 //***************************************************************************
-// Author: Henning Ødeby Karlsen                                            *
+// Author: Andreas Haugland                                         *
 //***************************************************************************
 
 // DUNE headers.
